b87d138ff126c821def33d67e513fe19e5a9a9c1
Adam Cygan <adamcyg772@student.polsl.pl>
2024-06-27 18:08:36 +0200

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { SignInButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p className="text=2xl">Home</p>
+    </div>
+  );
+};
+export default Home;
